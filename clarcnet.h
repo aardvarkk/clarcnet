054f82c03c810077485f96d71571fe2893091880
Ian Clarkson <clarksonian@gmail.com>
2016-07-08 21:29:43 -0400

    Showing better errors for a while

--- clarcnet.h
+++ clarcnet.h
@@ -23,15 +23,15 @@
 
 namespace clarcnet {
 
-	// #define thr \
-	// 	throw std::runtime_error(\
-	// 			std::string(__FILE__) + ":" +\
-	// 			std::to_string(__LINE__) + " " +\
-	// 			std::string(strerror(errno)));
+	 #define thr \
+	 	throw std::runtime_error(\
+	 			std::string(__FILE__) + ":" +\
+	 			std::to_string(__LINE__) + " " +\
+	 			std::string(strerror(errno)));
 
-	#define thr \
-		throw std::runtime_error(\
-				std::string(strerror(errno)));
+//	#define thr \
+//		throw std::runtime_error(\
+//				std::string(strerror(errno)));
 
 	#define chk(val) \
 	{\
